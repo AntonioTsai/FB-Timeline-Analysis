acdc9d3faa7db012dcb504f74a7d9b51de53fc0b
Antonio Tsai <jimmyjack7777@gmail.com>
2017-08-05 00:49:42 +0800

    Get 100 feed

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
 const client = require('./client');
-var {FB, FacebookApiException} = require('fb');
-var access_token = '';
+var { FB, FacebookApiException } = require('fb');
 
-// Get Access Token
-FB.api('oauth/access_token', {
-    client_id: client.id,
-    client_secret: client.secret,
-    grant_type: 'client_credentials'
-}, function (res) {
-    if(!res || res.error) {
+FB.api('me/feed', {
+    access_token: client.graph_api_explorer_token,
+    limit: 100,
+}, (res) => {
+    if (!res || res.error) {
         console.log(!res ? 'error occurred' : res.error);
         return;
     }
 
-    access_token = res.access_token;
-    // console.log(`${accessToken }`);
+    console.log(res);
+    // console.log(res.data.length);
 });

5b49383bd408e624ec3dee7a4c70fc9161c19e3c
Antonio Tsai <jimmyjack7777@gmail.com>
2017-08-06 22:30:56 +0800

    Check post_list.json and request feed if needed

--- index.js
+++ index.js
@@ -5,9 +5,40 @@ const graph = require('./graph_api');
 
 console.log('Please make sure "graph_api_explorer_token" has been updated!');
 
-// Check is there some data in `post_list.json`
-//  YES => Next Step
-//  NO => Get Feeds
+// Check is there any data in `post_list.json`
+const check_post_list = async () => {
+    // Todo Read JSON file `post_list.json`
+    if(fs.exists('./post_list.json')) {
+        var posts = require('./post_list.json');
+    } else {
+        posts = [];
+    }
+
+    if (posts.length == 0) {    //  Empty => Get feed
+        console.log('post_list.json is empty\n' +
+            'Request data from Facebook...');
+
+        try {   // Get feed
+            posts = await graph.getFeed();
+
+            // Store posts to post_list.json
+            data_stringify = JSON.stringify(posts);
+            // Save posts to post_list.json
+            fs.writeFile('post_list.json', data_stringify, err => {
+                if (err) {
+                    console.log(err);
+                }
+            });
+
+        } catch (err) {
+            console.log(err);
+        }
+    } else {
+        //  YES => Next Step
+    }
+}
+
+check_post_list()
 
 // Ask the user whether to update `post_list.json` or not
 //  YES => Update

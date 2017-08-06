51433fb02530da063688bfe8f9d6779d915b422e
Antonio Tsai <jimmyjack7777@gmail.com>
2017-08-06 16:00:24 +0800

    Make getFeed become a function

--- graph_api.js
+++ graph_api.js
@@ -1,24 +1,28 @@
+const config = require('./config');
 var { FB, FacebookApiException } = require('fb');
 
+exports.getFeed = () => new Promise(resolve => {
+    // TODO move limit, since(startDate) to parameter
+    FB.api('me/feed', {
+        access_token: config.graph_api_explorer_token,
+        limit: 100,
+        since: startDate
+    }, (res) => {
+        if (!res || res.error) {
+            console.log(!res ? 'error occurred' : res.error);
+            return;
+        }
 
-FB.api('me/feed', {
-    access_token: config.graph_api_explorer_token,
-    limit: 100,
-    since: startDate
-}, (res) => {
-    if (!res || res.error) {
-        console.log(!res ? 'error occurred' : res.error);
-        return;
-    }
-
-    if(res.data.length != 0) {
-        // Json to string
-        data_stringify = JSON.stringify(res.data);
-        // Save posts to post_list.json
-        fs.writeFile('post_list.json', data_stringify, err => {
-            if(err) {
-                console.log(err);
-            }
-        });
-    }
+        // TODO move this part to index.js
+        if (res.data.length != 0) {
+            // Json to string
+            data_stringify = JSON.stringify(res.data);
+            // Save posts to post_list.json
+            fs.writeFile('post_list.json', data_stringify, err => {
+                if (err) {
+                    console.log(err);
+                }
+            });
+        }
+    });
 });

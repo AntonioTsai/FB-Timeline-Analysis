c7c4fddb57ab6aac8d2236eac608b85658966b65
Antonio Tsai <jimmyjack7777@gmail.com>
2017-08-06 15:46:37 +0800

    Remove unnecessary require & variable

--- graph_api.js
+++ graph_api.js
@@ -1,7 +1,5 @@
-const fs = require('fs');
-const config = require('./config');
 var { FB, FacebookApiException } = require('fb');
-var startDate = '2017-01-01';
+
 
 FB.api('me/feed', {
     access_token: config.graph_api_explorer_token,

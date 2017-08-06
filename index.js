676f9c732ed14803c4c458e85aeb2ea812b3845c
Antonio Tsai <jimmyjack7777@gmail.com>
2017-08-06 15:42:52 +0800

    Add prompt before start the program

---
+++ index.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const config = require('./config');
+var startDate = '2017-01-01';
+
+
+// Prompt user to check the TOKEN has been updated
+console.log('Please make sure "graph_api_explorer_token" has been updated!\n' +
+'Press Enter to continue...');
+
+// Check is there some data in `post_list.json`
+//  YES => Next Step
+//  NO => Get Feeds
+
+// Ask the user whether to update `post_list.json` or not
+//  YES => Update
+//  NO => Get Feeds
+
+// Get all information of posts in `post_list.json`
+
+// Analysis

606d5288dd44ed0f6e5a80e2e568aa45948bd3e0
Antonio Tsai <jimmyjack7777@gmail.com>
2017-08-06 22:27:53 +0800

    Require graph_api

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const fs = require('fs');
 const config = require('./config');
-var startDate = '2017-01-01';
+const graph = require('./graph_api');
 
 
-// Prompt user to check the TOKEN has been updated
-console.log('Please make sure "graph_api_explorer_token" has been updated!\n' +
-'Press Enter to continue...');
+console.log('Please make sure "graph_api_explorer_token" has been updated!');
 
 // Check is there some data in `post_list.json`
 //  YES => Next Step
